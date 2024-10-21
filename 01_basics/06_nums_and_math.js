f9c414f00eee3eb1b4aaa174c002632a9d3a08d2
adarshchaudhary100 <147192892+adarshchaudhary100@users.noreply.github.com>
2024-10-21 19:46:05 +0000

    math and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,24 @@
+const score=34;
+const newscore=new Number(234.151);
+// console.log(score);
+// console.log(newscore);
+// console.log(newscore.toFixed(20));
+
+const anothernumber=264563.185;
+// console.log(anothernumber.toPrecision(4));
+
+const hundreds=1234243;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ***********math***************
+const num=5;
+// console.log(Math.round(422.49));
+// console.log(Math.min(23,52,626,272,16,1617,17));
+// console.log(Math.floor(Math.random()*10)+1);
+const min=10;
+const max=20;
+console.log(Math.floor((Math.random()*(max-min+1))+min));
+const mini=10;
+const maxi=20;
+console.log(Math.floor((Math.random()*(maxi-mini+1))+mini));
