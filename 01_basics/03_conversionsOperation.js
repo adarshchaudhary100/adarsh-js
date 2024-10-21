96656cc2522e399dd9a38b92ae554d63929d44eb
adarshchaudhary100 <147192892+adarshchaudhary100@users.noreply.github.com>
2024-10-21 16:35:11 +0000

    comparision

--- 01_basics/03_conversionsOperation.js
+++ 01_basics/03_conversionsOperation.js
@@ -18,3 +18,32 @@ console.log(somenumber);
 // console.log(stringchange);
 console.log(typeof(somenumber));
 // console.log(typeof stringchange);
+
+// ***********************operations******************
+ 
+let value=3;
+let negvalue=-value;
+// console.log(value);
+// console.log(negvalue);
+// console.log(value+negvalue);
+
+let str1="Adarsf";
+let str2=" akul";
+let str=str1+str2;
+// console.log(str);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(2+2+"1");
+// console.log(2+2*4%5+2);
+
+console.log(+" ");
+console.log(true+1);
+
+let num1=num2=num3=23;
+console.log(num1);
+
+let count=23;
+count++;
+console.log(count);
+console.log(++count);
+console.log(count++);
