519923dfe9454169c0de7d542cb4ef816d87a632
adarshchaudhary100 <147192892+adarshchaudhary100@users.noreply.github.com>
2024-10-22 17:54:24 +0000

    date time

---
+++ 01_basics/07_dateinJs.js
@@ -0,0 +1,14 @@
+let myDate=new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof(myDate));
+
+// let anotherDate= new Date(2023,13,24);
+let anotherDate= new Date("02-01-2024");
+// console.log(anotherDate.toDateString());
+
+let mytimestamp=Date.now();
+console.log(mytimestamp);
+console.log(anotherDate.getTime());
+console.log(anotherDate.getDate());
