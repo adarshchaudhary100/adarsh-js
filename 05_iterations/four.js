334efcc375fffe51dbb5d6800221864ccecf9ae4
adarshchaudhary100 <147192892+adarshchaudhary100@users.noreply.github.com>
2024-10-25 20:35:06 +0000

    forinofeach

---
+++ 05_iterations/four.js
@@ -0,0 +1,17 @@
+// forin loop*************
+
+const myobj={
+    js: 'javascript',
+    cpp: 'c++',
+    py: 'python'
+}
+
+// for (const key in myobj) {
+//     console.log(key, myobj[key]);
+// }
+
+
+const arr=[2,35,6,643,2];
+for (const key in arr) {
+    console.log(arr[key]);
+}
---
+++ 05_iterations/three.js
@@ -0,0 +1,35 @@
+// for of loop***************
+const arr=[1,2,4,56];
+// for (const element of arr) {
+//     console.log(element);
+// }
+
+// const string="2323245 afag";
+// for (const str of string) {
+//     console.log(str);
+// }
+
+
+// maps***************
+const map= new Map();
+map.set('india', 91);
+map.set(' usa', +42);
+map.set('can', +25);
+
+
+// console.log(map);
+
+// for (const [ele,val] of map) {
+//     console.log(ele,val);
+// }
+
+const myobj={
+    name: "adatrs", 
+    pta: "dasi",
+    mobile: "na hai"
+}
+
+// for (const ele of myobj){
+//     console.log(ele);
+// }
+// objects not iterate in forof loop in this type
