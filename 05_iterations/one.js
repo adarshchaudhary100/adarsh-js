423e0a3d0b39911aa1804b7795daa596d5c5e363
adarshchaudhary100 <147192892+adarshchaudhary100@users.noreply.github.com>
2024-10-25 19:44:03 +0000

    loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,10 @@
+// for loop****************
+
+for (let index = 0; index < 10; index++) {
+    const element =index;
+    console.log(index);
+}
+
+// break-> break full loop 
+// continue: except one condition
+//   i know these as well
---
+++ 05_iterations/two.js
@@ -0,0 +1,13 @@
+// while loop
+
+while(true){
+    console.log("Print krdo adarah");
+    break;
+}
+
+// ye aata hai
+ let a=12;
+ do{
+    console.log(`${a}`);
+    a++;
+ }while(a<10);
