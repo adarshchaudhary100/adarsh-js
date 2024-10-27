759e34567cfb90ac6ee60af5b76ed70ba9645401
adarshchaudhary100 <147192892+adarshchaudhary100@users.noreply.github.com>
2024-10-27 04:47:57 +0000

    foreach

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -1,1 +1,39 @@
 // foreach loop****************
+ const coding =[ "js ", "ruby", "c++", "python"]
+ 
+//  coding.forEach(function(item){
+//         console.log(item);
+//  })
+
+// coding.forEach((val)=>{
+//     console.log(val);
+// })
+
+// function printme(item){
+//     console.log(item);
+// }
+// coding.forEach(printme);
+
+// coding.forEach((item,index,arr)=>{
+//     console.log(item, index, arr);
+// })
+
+
+const mycoding=[
+    {
+        firstlang: "bhailang",
+        type: "bhai"
+    },
+    {
+        firstlang: "javascript",
+        type: "js"
+    },
+    {
+        firstlang: "c++",
+        type: "cpp"
+    }
+]
+
+mycoding.forEach(function(obj){
+    console.log(obj.firstlang);
+})
