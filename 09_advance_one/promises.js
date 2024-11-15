2c0411b5c0bc95632f2324f46130ccf443ac141d
adarshchaudhary100 <147192892+adarshchaudhary100@users.noreply.github.com>
2024-11-15 21:36:34 +0000

    fetch

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -33,11 +33,73 @@
 // })
 
 
-const promiseFour=new Promise(function(resolve,reject){
+// const promiseFour=new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         let error=false;
+//         if(!error){
+//             resolve({username:"adarsh", password:"123456"})
+//             // resolve()
+//         }
+//         else{
+//             reject(' ERROR: something went wrong');
+//         }
+//     },1000);
+// })
+
+// promiseFour.then((user)=>{
+//     console.log(user);
+//     return user.username;
+// }).then((username)=>{
+//      console.log(username)
+// } ).catch(function(error){
+//     console.log(error)
+// }).finally(()=>console.log('Finally finished'));
+
+// console.log(username);
+
+const promiseFive= new Promise(function(resolve,reject){
     setTimeout(function(){
         let error=true;
         if(!error){
-            resolve({username:"adarsh", password:"123456"})
+            // console.log('')
+            resolve({username:"Akul", password: "38452"});
         }
-    },1000);
+        else{
+            reject("poam went wrong");
+        }
+    },1000)
+})
+
+async function consumePromiseFive(){
+    try {
+        const response=await promiseFive;
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}   
+consumePromiseFive()
+
+// async function getAllUsers(){
+//     try {
+//         const response=await fetch('https://jsonplaceholder.typicode.com/users')
+//         // console.log(response);
+//         const data=await response.json();
+//         console.log(data);
+
+//     } catch (error) {
+//         console.log('E: ', error);   
+//     }
+// }
+// getAllUsers();.
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response)=>{
+    return response.json();
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch((error)=>{
+    console.log(error);
 })
